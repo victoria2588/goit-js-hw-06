66e4a4ab8518b2febf92dfb97c6d43f12ef86dc4
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-04 14:36:46 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const ttt = ingredients.map((el) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = el;
+  return liEl;
+});
+listEl.append(...ttt);
