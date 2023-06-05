2825facacc713644763aee6e7a6b565bc4012e5c
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-05 19:02:41 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", submitHandler);
+
+const profile = {};
+
+function submitHandler(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email;
+  const password = event.currentTarget.elements.password;
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    profile.email = email.value;
+    profile.password = password.value;
+  }
+  console.log(profile);
+  event.currentTarget.reset();
+}
