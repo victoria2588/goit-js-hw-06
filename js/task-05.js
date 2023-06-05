4943b8576e9881206ef200023ead672537ba5d54
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-05 14:18:49 +0300

    task-5

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", (event) => {
+  outputEl.textContent =
+    event.currentTarget.value === "" ? "Anonymous" : event.currentTarget.value;
+});
