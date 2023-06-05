e7086d4fc2e3999bedb83e0f166111c964119c58
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-05 19:18:30 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+const bodyEl = document.querySelector(".widget");
+
+btnEl.addEventListener("click", function () {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+});
